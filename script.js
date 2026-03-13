e7c8a3f066e5178f8d6702e4786df33785a24174
Risako Morikawa <moriko3970@gmail.com>
2026-03-13 19:37:08 +0900

    script

---
+++ script.js
@@ -0,0 +1,9 @@
+let count = 0;
+
+const button = document.getElementById("btn");
+const display = document.getElementById("count");
+
+button.addEventListener("click", () => {
+  count = count + 1;
+  display.textContent = count;
+});
